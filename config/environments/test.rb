8ab3171c0a3621b75a065edff112816d7a237a61
tinosam <yvestinos@gmail.com>
2019-01-27 22:51:25 +0300

    test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,8 @@ Rails.application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
-
+  ENV["AWS_ACCESS_KEY_ID"] = "AKIAI367JHRCZ2XUVTDA"
+  ENV["AWS_SECRET_ACCESS_KEY"] = "Gb7Gxx8MkXcxDbYQR6vGuWK44tIH9IaBGIs3VXo0"
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
